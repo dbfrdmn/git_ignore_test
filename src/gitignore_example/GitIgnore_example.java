fb500a71ae970648bee873de6d25e3fa810f37a7
Debbie <Debbie@192.168.120.222>
2016-10-19 11:01:22 -0500

    Copied in info from gitignore.io

--- src/gitignore_example/GitIgnore_example.java
+++ src/gitignore_example/GitIgnore_example.java
@@ -8,6 +8,8 @@ public class GitIgnore_example {
 
     public static void main(String[] args) {
 System.out.println("This is GitIgnore");
-    System.out.println("Don't forget to create a .gitignore file.");}
+    System.out.println("Don't forget to create a .gitignore file.");
+    System.out.println("Ignore About.java");
+    }
     
 }
