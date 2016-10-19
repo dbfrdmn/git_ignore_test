94ee6cd63e95fad95686140d89c6ef0b8fd3fd22
Debbie <Debbie@192.168.120.222>
2016-10-19 10:57:32 -0500

    Copied in info from gitignore.io

---
+++ src/gitignore_example/GitIgnore_example.java
@@ -0,0 +1,13 @@
+/* Debbie Friedman
+GitIgnore example
+October 19, 2016
+ */
+package gitignore_example;
+
+public class GitIgnore_example {
+
+    public static void main(String[] args) {
+System.out.println("This is GitIgnore");
+    System.out.println("Don't forget to create a .gitignore file.");}
+    
+}
